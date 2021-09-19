b0e72d633c8f0aab8424ebdf685c29615ae26d4a
pranjal-prateek <81037562+pranjal-prateek@users.noreply.github.com>
2021-09-19 11:42:57 +0530

    second commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,74 +1,58 @@
 import React, { Component } from 'react'
+import Spinner from './Spinner';
 import Newsitem from './Newsitem'
 
 export class News extends Component {
-    articles =  [
-        {
-        "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-        "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-        ];
+    
     constructor(){
         super();
-        console.log("hello sexy i am a constructor from news");
+        // console.log("hello sexy i am a constructor from news");
         this.state={
-            articles:this.articles,
-            loading:false
+            articles:[],
+            loading:false,
+            page:1,
+
 
         }
     }
     async componentDidMount(){
-    console.log("cdm");
-        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=42f09ac2a50e43ab8a4dccd0e485e70f&page=1";
+    // console.log("cdm");
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=42f09ac2a50e43ab8a4dccd0e485e70f&page=1&pageSize=${this.props.pageSize}`;
         let data=await fetch(url);
         let parsedData=await data.json();
         console.log(parsedData);
-        this.setState({articles:parsedData.articles});
+        this.setState({articles:parsedData.articles,totalArticles:parsedData.totalResults});
 
     }
     handleprevious=async()=>{
 console.log("previous");
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=42f09ac2a50e43ab8a4dccd0e485e70f&page=${this.state.page -1}`;
+    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=42f09ac2a50e43ab8a4dccd0e485e70f&page=${this.state.page -1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data=await fetch(url);
     let parsedData=await data.json();
-    console.log(parsedData);
+    this.setState({loading:false})
+   
     this.setState({articles:parsedData.articles});
     this.setState({
         page:this.state.page -1,
+        articles:parsedData.articles,
+        loading:false
 })
     }
      handlenext=async()=>{
         console.log("next");
-        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=42f09ac2a50e43ab8a4dccd0e485e70f&page=${this.state.page+1}`;
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=42f09ac2a50e43ab8a4dccd0e485e70f&page=${this.state.page+1}pageSize=${this.props.pageSize}`;
+        this.setState({loading:true})
+
         let data=await fetch(url);
         let parsedData=await data.json();
-        console.log(parsedData);
+    this.setState({loading:false})
+        
         this.setState({articles:parsedData.articles});
         this.setState({
             page:this.state.page+1,
             articles:parsedData.articles,
+            loading:false
         })
     }
     render() {
@@ -76,7 +60,8 @@ console.log("previous");
         return (
             <>
             <div className="container my-3">
-                <h2>NewsBuddy -Top headline</h2>
+            <h1 className="text-center">NewsBuddy -Top headline</h1>
+            {this.state.loading&&<Spinner/>}
                 
             <div className="row">
             {this.state.articles.map((element)=>{
